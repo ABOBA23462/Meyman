2c106017fd0b07c399663d0a4c5feab9e6e29b29
UrazbaevT <Utilek990@gmail.com>
2023-08-29 16:57:11 +0600

    bookingFragment & BookingDetailFragment layout's

--- app/src/main/java/com/example/meyman/core/Сonstants.kt
+++ app/src/main/java/com/example/meyman/core/Сonstants.kt
@@ -1,7 +1,7 @@
 package com.example.meyman.core
 
 object Constant {
-    const val BASE_URL = "https://6c98-185-53-231-243.ngrok-free.app"
+    const val BASE_URL = "http://meyman.tw1.ru/"
 
     const val DEFAULT_ID = 0
 }
---
+++ app/src/main/java/com/example/meyman/presentation/ui/screens/booking/booking_detail/BookingDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.meyman.presentation.ui.screens.booking.booking_detail
+
+import androidx.lifecycle.ViewModel
+
+class BookingDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
